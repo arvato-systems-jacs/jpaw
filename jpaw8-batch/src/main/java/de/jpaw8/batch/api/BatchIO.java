62205d81c6c1cae010a018770c8945dda2688ab5
Michael Bischoff <Michael.Bischoff@bertelsmann.de>
2026-06-17 12:08:19 +0200

    JPAW-17: version change and cleanup, part 1

--- jpaw8-batch/src/main/java/de/jpaw8/batch/api/BatchIO.java
+++
@@ -1,6 +0,0 @@
-package de.jpaw8.batch.api;
-
-public interface BatchIO {
-    default void open() throws Exception { }
-    default void close() throws Exception { }
-}
--- jpaw8-batch/src/main/java/de/jpaw8/batch/api/BatchMarshallerFactory.java
+++
@@ -1,6 +0,0 @@
-package de.jpaw8.batch.api;
-
-@FunctionalInterface
-public interface BatchMarshallerFactory<X> {
-    BatchMarshaller<X> getMarshaller(int threadNo);
-}
